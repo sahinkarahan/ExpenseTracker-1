11918c642176a4fb493af0d16d879f25b8d46e97
sahinkarahan <sahin.karahan@hotmail.com>
2025-01-01 11:39:40 +0300

    Initial Commit

---
+++ ExpenseTracker-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTracker-1
+//
+//  Created by Şahin Karahan on 1.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
